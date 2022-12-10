04314aacaf774b8ec8b14e1a236d311764a927eb
Alexander Hortua <alexander.hortua10@gmail.com>
2022-12-10 15:36:19 -0800

    Removing unnecesary files

--- src/sanctuary/SanctuaryKeeperDriver.java
+++ src/sanctuary/SanctuaryKeeperDriver.java
@@ -4,14 +4,8 @@ import sanctuary.controller.SanctuaryController;
 import sanctuary.controller.SanctuaryFeatures;
 import sanctuary.model.habitat.Habitat;
 import sanctuary.model.habitat.Sanctuary;
-import sanctuary.utils.Food;
-import sanctuary.utils.Sex;
-import sanctuary.utils.Species;
 import sanctuary.view.SanctuaryAnimalView;
 import sanctuary.view.SanctuaryView;
-
-import java.io.*;
-import java.util.Scanner;
 
 
 /**
